171fdf1b99dff70899ee8c6f9689cc06bb936ef9
Koua Thao <kthao91@live.com>
2025-07-29 17:13:25 -0500

    Create 10-RegularExpressionMatching

---
+++ 10-RegularExpressionMatching/version/java/10-RegularExpressionMatching_20250729_171325.java
@@ -0,0 +1,40 @@
+// Last updated: 7/29/2025, 5:13:25 PM
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+     // dp[i][j] will be true if s[0..i-1] matches p[0..j-1]
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true; // empty string matches empty pattern
+
+        // Handle patterns like a*, a*b*, a*b*c* that can match empty string
+        for (int j = 2; j <= n; j += 2) {
+            if (p.charAt(j - 1) == '*' && dp[0][j - 2]) {
+                dp[0][j] = true;
+            }
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if (pc == '.' || pc == sc) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    // zero occurrence of the preceding element
+                    dp[i][j] = dp[i][j - 2];
+
+                    // one or more occurrence if the preceding character matches
+                    char prev = p.charAt(j - 2);
+                    if (prev == '.' || prev == sc) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }   
+}
